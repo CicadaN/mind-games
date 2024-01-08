c7aea1827f28eb6685289540407a80e7b630c5f3
CicadaN <88105680+CicadaN@users.noreply.github.com>
2024-01-08 17:46:05 +0300

    part 2, up App class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
+    application
     id("java")
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application { mainClass.set("hexlet.code.App") }
+
 
 repositories {
     mavenCentral()
